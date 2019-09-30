928fd1216c8ccd3068d96a923ce80769b9a018c3
James Leung <jleung128hk@gmail.com>
2019-09-30 17:10:13 -0400

    First commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var router = express.Router();
+
+// Import movies array
+const movies = require("../data/movies");
+
+/* GET home page. */
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/most_popular", (req, res, next) => {
+  let page = req.query.page;
+  if (req.query.api_key != 1234) {
+    res.json("Invalid API key");
+  } else {
+    let results = movies.filter(movie => {
+      return movie.most_popular;
+    });
+    const indexToStart = (page - 1) * 20;
+    results = results.slice(indexToStart, indexToStart + 19);
+    res.json({ results });
+  }
+});
+
+module.exports = router;
